d161f33bbb1f00d3cb9962f18244548b5211e907
hendrik <24483010+kwagga333@users.noreply.github.com>
2023-08-13 20:53:54 +0200

    Add Webpack structure and setup

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import preload from './pre-load';
+import './style.css';
+
+
+preload();
---
+++ src/pre-load.js
@@ -0,0 +1,13 @@
+
+
+const preload =() => {
+
+const content = document.querySelector("#content");
+
+content.innerHTML = ` <h1>"Hello World"</h1> `;
+
+};
+
+
+
+export default preload;
